844be4ee1197df1a6396c51f375f65b058e0d1d0
Mo Amin <m.alamin92@gmail.com>
2017-07-12 20:40:21 -0400

    prettier

--- src/components/AlbumWall.js
+++ src/components/AlbumWall.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+/* global localStorage */
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class AlbumWall extends Component {
   constructor(props) {
@@ -10,22 +11,28 @@ export default class AlbumWall extends Component {
   }
   componentWillMount() {
     let that = this;
-    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('spotify-access-token')}`;
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.getItem("spotify-access-token")}`;
     axios
-      .get('https://api.spotify.com/v1/me/top/tracks?limit=48')
+      .get("https://api.spotify.com/v1/me/top/tracks?limit=48")
       .then(data => that.setState({ tracks: data.data.items }))
       .catch(e => console.log(e));
   }
   render() {
     console.log(this.state);
-    let albums = this.state.tracks ?
-      this.state.tracks.map( (item) => {
-        return (
-          <div key={item.id} className="col-3">
-            <img className="img-fluid" src={item.album.images[0].url} alt={item.album.url}/>
-          </div>
-        )
-      })
+    let albums = this.state.tracks
+      ? this.state.tracks.map(item => {
+          return (
+            <div key={item.id} className="col-3">
+              <img
+                className="img-fluid"
+                src={item.album.images[0].url}
+                alt={item.album.url}
+              />
+            </div>
+          );
+        })
       : [];
 
     return this.state.tracks
